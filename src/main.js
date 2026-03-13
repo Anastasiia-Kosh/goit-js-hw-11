8e35acd3585d063416c92dfa70161fda8284acd3
Анастасія Кошуцька <zaporozhetsnastya89@gmail.com>
2026-03-13 15:00:11 +0200

    hw-11

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+const userForm = document.querySelector('.form');
+
+userForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const userSearch = formData.get('search-text').trim();
+  if (!userSearch) return;
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(userSearch)
+    .then(data => {
+      if (!data.hits.length) {
+        iziToast.show({
+          message: 'Sorry, there are no images matching your search query.',
+          position: 'topRight',
+          backgroundColor: '#ef4040',
+          messageColor: 'white',
+        });
+        return;
+      }
+      createGallery(data.hits);
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      hideLoader();
+    });
+});
